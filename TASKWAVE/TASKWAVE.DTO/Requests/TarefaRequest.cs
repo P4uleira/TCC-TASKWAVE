3fc70dee6f33c9ffbdf4b05f04aa72cef2fd0703
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2025-06-24 22:59:05 -0300

    fix: Ajuste do TarefaRequest

--- TASKWAVE/TASKWAVE.DTO/Requests/TarefaRequest.cs
+++ TASKWAVE/TASKWAVE.DTO/Requests/TarefaRequest.cs
@@ -2,5 +2,5 @@
 
 namespace TASKWAVE.DTO.Requests
 {
-    public record TarefaRequest(string taskName, string? taskDescription, SituacaoTarefaEnum taskStatus, PrioridadeTarefaEnum taskPriority, DateTime taskCreationDate, int projectId);
+    public record TarefaRequest(string taskName, string? taskDescription, SituacaoTarefaEnum taskStatus, PrioridadeTarefaEnum taskPriority, DateTime taskCreationDate, DateTime? taskPlannedDate, DateTime? taskFinalDate, int projectId);
 }
